b4a998bb00bb1c12df3b71ca7efdf835daac8f94
Andoni Alonso <andonialonsof@gmail.com>
2017-10-01 20:45:55 +0200

    Initial Commit

---
+++ DefGrafana.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# [ES] DefGrafana.py - Script para capturar imagenes dentro de Grafana.
+#
+# Modo de uso: - Modificar seccion "VARS"
+#              - Ejecutar: python3 DefGrafana.py '<URL Dashboard>'
+#
+# https://github.com/andoniaf
+
+#### VARS ####
+username = 'admin'
+passGraf = 'secret'
+# Necesario sobretodo en consultas pesadas
+timeout = 5
+imgName = 'webScreen_'
+##############
+import sys
+import time
+from selenium import webdriver   
+from selenium.webdriver.common.keys import Keys
+
+if len(sys.argv) == 1:
+  mensaje = "Uso:\n - Modificar seccion \"VARS\"\n"
+  mensaje += " - Ejecutar: python3 DefGrafana.py '<URL Dashboard>'"
+  print(mensaje)
+
+else:
+  webUrl = sys.argv[1] 
+  print("URL a capturar: " + webUrl)
+
+  driver = webdriver.PhantomJS()
+  driver.get(webUrl)
+
+  ## Introducimos username
+  user_name=driver.find_element_by_name('username')
+  user_name_text = user_name.text
+  user_name.clear()
+  user_name.send_keys(username)
+  
+  ## Introducimos password
+  password=driver.find_element_by_id('inputPassword')
+  password.clear()
+  password.send_keys(passGraf)
+  password.send_keys(Keys.ENTER)
+  
+  ## Espera a que cargue la consulta
+  time.sleep(timeout)
+  
+  ## Screenshot
+  # Timestamp para evitar sobreescribir capturas
+  currTime = time.strftime("%y%m%d%H%M%S", time.localtime())
+  imgName = imgName + currTime + '.png'
+  driver.save_screenshot(imgName)
+  print("Screen guardada como: " + imgName)
+
--- README.md
+++ README.md
@@ -1,2 +1,43 @@
+<img src="./defGrafana.png" width="300">
 # DefGrafana.py
-Script para capturar imagenes dentro de Grafana.
+
+Script para capturar imágenes dentro de Grafana.
+
+
+
+## Modo de uso
+- Modificar sección "VARS" dentro del script.
+- Ejecutar: ```python3 DefGrafana.py '<URL a capturar>'```
+
+
+
+## Dependencias
+- **Python**: Probado con las versiones 3.4.2 y 3.5.2
+  - Dependencias: ```pip3 install -r requirements.txt```
+- **phantomJS**: Versión 2.1.1
+  - Ver [Anexo I](#Anexo-I) para instalación sobre Ubuntu/Debian
+
+
+
+
+
+
+### Origen del nombre
+* **Grafana**: Fácil, se usa con [Grafana](https://grafana.com/).
+* **Def**: Si aun no has caído [pincha aqui](https://open.spotify.com/album/0IXPDVnECWSt6NFLDlgpoC).
+
+
+
+#### Anexo I: instalación de PhantomJS en Ubuntu/Debian
+```
+apt-get update
+apt-get upgrade
+apt-get install libfreetype6 libfreetype6-dev
+apt-get install build-essential chrpath libssl-dev libxft-dev
+apt-get install libfreetype6 libfreetype6-dev
+apt-get install libfontconfig1 libfontconfig1-dev
+wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
+tar xvjf phantomjs-2.1.1-linux-x86_64.tar.bz2
+mv phantomjs-2.1.1-linux-x86_64 /usr/local/share
+ln -sf /usr/local/share/phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/local/bin
+```
